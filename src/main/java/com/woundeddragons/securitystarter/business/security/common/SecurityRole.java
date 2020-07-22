2b2540db649e4b3cd475cb330a0ef889d9e2c106
marcelomnc <marcelo@pig.gi>
2020-07-22 11:11:14 -0500

    Component addition to encapsulate security roles. :bowtie:

---
+++ src/main/java/com/woundeddragons/securitystarter/business/security/common/SecurityRole.java
@@ -0,0 +1,32 @@
+package com.woundeddragons.securitystarter.business.security.common;
+
+public enum SecurityRole {
+    ROLE_2FA_CODE_VERIFICATION(-1, Names.ROLE_2FA_CODE_VERIFICATION),
+    //TODO: Adjust your app roles as needed !
+    ROLE_ADMIN(1, Names.ROLE_ADMIN),
+    ROLE_USER(2, Names.ROLE_USER);
+
+    private SecurityRole(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public static class Names {
+        private static final String ROLE_PREFIX = "ROLE_";
+
+        public static final String ROLE_2FA_CODE_VERIFICATION = ROLE_PREFIX + "2FA_CODE_VERIFICATION";
+        public static final String ROLE_ADMIN = ROLE_PREFIX + "ADMIN";
+        public static final String ROLE_USER = ROLE_PREFIX + "USER";
+    }
+}
