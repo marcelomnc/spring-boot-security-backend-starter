18b92e37bd16288849986c85718a55053cbdc00a
marcelomnc <marcelo@pig.gi>
2020-08-11 12:11:03 -0500

    Request validation constraints addition. :bowtie:

--- src/main/java/com/woundeddragons/securitystarter/web/api/v1/request/AuthenticationRequest.java
+++ src/main/java/com/woundeddragons/securitystarter/web/api/v1/request/AuthenticationRequest.java
@@ -4,14 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Getter
 @Setter
 public class AuthenticationRequest implements Serializable {
     @NotEmpty(message = "You must send a username with this request.")
+    @Size(min = 5, max = 255)
     private String username;
 
     @NotEmpty(message = "You must send a password with this request.")
+    @Size(min = 1, max = 255)
     private String password;
 }
