602ba358d71347c3b6ea6fe0a5d16029abc3445a
marcelomnc <marcelo@pig.gi>
2020-09-08 10:55:14 -0500

    Default password change for one that accomplishes the rules. :bowtie:

--- src/main/java/com/woundeddragons/securitystarter/util/BCryptEncoderTool.java
+++ src/main/java/com/woundeddragons/securitystarter/util/BCryptEncoderTool.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 final class BCryptEncoderTool {
     public static void main(String[] args) {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        String password = "abcde$$12345";
+        String password = "MyPassword$09";
         String hashedPassword = bCryptPasswordEncoder.encode(password);
         System.out.println("Original password: '" + password + "'.");
         System.out.println("Hashed password: '" + hashedPassword + "'.");
