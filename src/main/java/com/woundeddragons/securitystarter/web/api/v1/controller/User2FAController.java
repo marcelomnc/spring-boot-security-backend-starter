a214e08627ccd096774f30e11c2d40704728e1aa
marcelomnc <marcelo@pig.gi>
2020-09-09 08:57:02 -0500

    Logic refactoring to use common authentication delegate component. :bowtie:

--- src/main/java/com/woundeddragons/securitystarter/web/api/v1/controller/User2FAController.java
+++ src/main/java/com/woundeddragons/securitystarter/web/api/v1/controller/User2FAController.java
@@ -2,15 +2,16 @@ package com.woundeddragons.securitystarter.web.api.v1.controller;
 
 import com.woundeddragons.securitystarter.business.common.SecurityRole;
 import com.woundeddragons.securitystarter.business.model.CustomUserDetails;
+import com.woundeddragons.securitystarter.web.api.common.delegate.AuthenticationDelegate;
+import com.woundeddragons.securitystarter.web.api.common.response.ErrorsEnum;
 import com.woundeddragons.securitystarter.web.api.v1.Constants;
 import com.woundeddragons.securitystarter.web.api.v1.request.T2FACodeVerificationRequest;
 import com.woundeddragons.securitystarter.web.api.v1.response.AuthenticationResponse;
-import com.woundeddragons.securitystarter.web.api.common.AuthHelper;
-import com.woundeddragons.securitystarter.web.api.common.response.ErrorsEnum;
 import org.jboss.aerogear.security.otp.Totp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -30,12 +31,12 @@ public class User2FAController {
     public static final String PATH = "/user/2fa";
 
     @Autowired
-    private AuthHelper authHelper;
+    private AuthenticationDelegate authenticationDelegate;
 
     @PostMapping(path = PATH)
     public ResponseEntity<AuthenticationResponse> doVerification(@RequestBody @Valid T2FACodeVerificationRequest t2FACodeVerificationRequest) {
-        ResponseEntity toRet = null;
-        AuthenticationResponse authenticationResponse = null;
+        ResponseEntity<AuthenticationResponse> toRet;
+        AuthenticationResponse authenticationResponse;
 
         logger.debug("T2FACodeVerificationController verifying 2FA code '" + t2FACodeVerificationRequest.getT2FACode() + "'.");
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -44,13 +45,13 @@ public class User2FAController {
         //Verify 2fa
         Totp totp = new Totp(customUserDetails.getUser().getDs2faSecret());
         if (totp.verify(t2FACodeVerificationRequest.getT2FACode())) {
-            authenticationResponse = this.authHelper.doAuthentication(customUserDetails.getUser(), true);
+            authenticationResponse = this.authenticationDelegate.doAuthentication(customUserDetails.getUser(), true);
             toRet = ResponseEntity.ok().body(authenticationResponse);
         } else {
             authenticationResponse = new AuthenticationResponse();
             authenticationResponse.addResponseError(ErrorsEnum.INVALID_2FA_CODE);
             authenticationResponse.setMustVerify2FACode(true);
-            toRet = ResponseEntity.badRequest().body(authenticationResponse);
+            toRet = ResponseEntity.status(HttpStatus.NOT_FOUND).body(authenticationResponse);
         }
 
         return toRet;
