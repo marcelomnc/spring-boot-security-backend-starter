8f0714fbc319ce305ebe403f25cee84fcfaa3f14
marcelomnc <marcelo@pig.gi>
2020-09-09 08:52:47 -0500

    Logic and components addition to use a common component for the JWT processing. :bowtie:

--- src/main/java/com/woundeddragons/securitystarter/web/api/common/filter/JWTProcessorFilter.java
+++ src/main/java/com/woundeddragons/securitystarter/web/api/common/filter/JWTProcessorFilter.java
@@ -1,22 +1,19 @@
 package com.woundeddragons.securitystarter.web.api.common.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.woundeddragons.securitystarter.business.model.CustomUserDetails;
-import com.woundeddragons.securitystarter.business.service.CustomUserDetailsService;
 import com.woundeddragons.securitystarter.web.api.Constants;
+import com.woundeddragons.securitystarter.web.api.common.WebSecurityConstants;
+import com.woundeddragons.securitystarter.web.api.common.delegate.JWTProcessorDelegate;
 import com.woundeddragons.securitystarter.web.api.common.response.BaseResponse;
 import com.woundeddragons.securitystarter.web.api.common.response.ErrorsEnum;
-import com.woundeddragons.securitystarter.web.api.common.JWTUtils;
-import com.woundeddragons.securitystarter.web.api.common.WebSecurityConstants;
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
@@ -28,14 +25,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @Component
 public class JWTProcessorFilter extends OncePerRequestFilter {
     @Autowired
-    private CustomUserDetailsService customUserDetailsService;
+    private JWTProcessorDelegate jwtProcessorDelegate;
 
     @Autowired
     private ServletContext servletContext;
@@ -54,10 +48,10 @@ public class JWTProcessorFilter extends OncePerRequestFilter {
         String header = req.getHeader(WebSecurityConstants.HEADER_STRING);
         String jwt = header.substring(WebSecurityConstants.JWT_PREFIX.length());
         try {
-            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = buildAuthenticationFromJWT(jwt);
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = this.jwtProcessorDelegate.buildAuthenticationFromJWT(jwt, true);
             SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             fc.doFilter(req, res);
-        } catch (UsernameNotFoundException | ExpiredJwtException | MalformedJwtException | SignatureException e) {
+        } catch (AuthenticationException | JwtException e) {
 
             ErrorsEnum errorsEnum;
             if (e instanceof UsernameNotFoundException) {
@@ -88,38 +82,5 @@ public class JWTProcessorFilter extends OncePerRequestFilter {
             res.setContentType("application/json");
             res.getOutputStream().write(jacksonObjectMapper.writeValueAsBytes(toRet));
         }
-    }
-
-    private UsernamePasswordAuthenticationToken buildAuthenticationFromJWT(String jwt) {
-        Claims claims = JWTUtils.parseClaims(jwt);
-        String username = claims.getSubject();
-        CustomUserDetails customUserDetails = (CustomUserDetails) this.customUserDetailsService.loadUserByUsername(username);
-
-        if (!customUserDetails.isCredentialsNonExpired()) {
-            throw new CredentialsExpiredException(null);
-        }
-
-        if (!customUserDetails.isAccountNonLocked()) {
-            throw new LockedException(null);
-        }
-
-        if (!customUserDetails.isEnabled()) {
-            throw new DisabledException(null);
-        }
-
-        if (!customUserDetails.isAccountNonExpired()) {
-            throw new AccountExpiredException(null);
-        }
-
-        List<String> securityRolesList = JWTUtils.getSecurityRoles(claims);
-        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-        if (securityRolesList != null && !securityRolesList.isEmpty()) {
-            securityRolesList.forEach(roleName -> {
-                grantedAuthorities.add(new SimpleGrantedAuthority(roleName));
-                logger.debug("Added Granted Authority: " + roleName);
-            });
-        }
-
-        return new UsernamePasswordAuthenticationToken(customUserDetails, null, grantedAuthorities);
     }
 }
